85799228dc1ef8668dccbf67eb035fcff449f3ac
반성욱 <bsu9370@naver.com>
2022-12-08 14:49:22 +0900

    최초 커밋

---
+++ src/test/java/com/example/baneureka/BanEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baneureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
